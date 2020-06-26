109da8dc47b7a96f0db993b064c379fefe581a75
Elam Day <elam.day.friedland@gmail.com>
2020-06-25 17:19:37 -0700

    workin on being able to make grid cells into numpy array of binary

--- pic2maze.py
+++ pic2maze.py
@@ -1,19 +1,13 @@
 import cv2 as cv
 import numpy as np
-
 image  = cv.imread("sudoku.jpg")
-cv.imshow("Image", image)
 
 gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-cv.imshow("gray", gray)
 
 blur = cv.GaussianBlur(gray, (5,5), 0)
-cv.imshow("blur", blur)
 
 thresh = cv.adaptiveThreshold(blur, 255, 1, 1, 11, 2)
-cv.imshow("thresh", thresh)
 contours, _ = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-
 max_area = 0
 c = 0
 for i in contours:
@@ -22,23 +16,32 @@ for i in contours:
                 if area > max_area:
                     max_area = area
                     best_cnt = i
-                    image = cv.drawContours(image, contours, c, (0, 255, 0), 3)
+                #    image = cv.drawContours(image, contours, c, (0, 255, 0), 3)
         c+=1
+lowest = 10000000000
+highest = 0
+rectcoords = [[],[]]
+for i in best_cnt:
+    if not i[0][1]:
+        continue
+    if i[0][0] + i[0][1] < lowest:
+        rectcoords[0] = [i[0][0],i[0][1]]
+        lowest = i[0][0] + i[0][1]
+    elif i[0][0] + i[0][1] > highest:
+        rectcoords[1] = [i[0][0],i[0][1]]
+        highest = i[0][0] + i[0][1]
+cv.rectangle(image,(rectcoords[0][0], rectcoords[0][1]),(rectcoords[1][0],rectcoords[1][1]),3, 4)
 
 mask = np.zeros((gray.shape),np.uint8)
 cv.drawContours(mask,[best_cnt],0,255,-1)
 cv.drawContours(mask,[best_cnt],0,0,2)
-cv.imshow("mask", mask)
 
 out = np.zeros_like(gray)
 out[mask == 255] = gray[mask == 255]
-cv.imshow("New image", out)
 
 blur = cv.GaussianBlur(out, (5,5), 0)
-cv.imshow("blur1", blur)
 
 thresh = cv.adaptiveThreshold(blur, 255, 1, 1, 11, 2)
-cv.imshow("thresh1", thresh)
 
 contours, _ = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 
@@ -48,8 +51,8 @@ for i in contours:
         if area > 1000/2:
             cv.drawContours(image, contours, c, (0, 255, 0), 3)
         c+=1
-
-
+print(rectcoords)
+image = image[rectcoords[0][0]:rectcoords[1][0],rectcoords[0][1]:rectcoords[1][1]]
 cv.imshow("Final Image", image)
 cv.waitKey(0)
 cv.destroyAllWindows()
